10dcf9d0132709702dc02ae3ae87efe9cf6359dc
Dima <dpotap247@gmail.com>
2020-01-24 13:30:07 +0200

    add axios

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="index">
+    <v-data-table
+    :headers="headers"
+    :items="desserts"
+    :items-per-page="5"
+    class="elevation-1">
+    </v-data-table>
   </div>
 </template>
 
@@ -17,9 +22,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
+
 ul {
   list-style-type: none;
   padding: 0;
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router.js'
+import axios from 'axios'
+
+Vue.prototype.$axios = axios
+
 Vue.config.productionTip = false
 
 new Vue({
